2490ac9505e184d3b9abbb40c201564a86050819
trentsalas <tes1006@uncw.edu>
2023-02-08 18:33:53 -0500

    fixed db to hold user info

--- povpoker/app.py
+++ povpoker/app.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template, request, Response, session, redirect
+from flask import Flask, render_template, request, Response, session, redirect, flash
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, validators
 from main import Game
 from Player import Player
 from User import User
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///Users/yttfk/desktop/POV-Poker/povpoker/user.db'
 db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = "my super secret key that no one is supposed to know"
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
@@ -22,6 +24,27 @@ player = [Player("Jeremy",None,None,1000,0,0), Player("Matt",None,None,1000,1,0)
 game = Game(player, 10, 20)
 game.newRound()
 
+class UserForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = StringField("Password", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+@app.route('/addUser', methods=['GET', 'POST'])
+def add_user():
+    username = None
+    form = UserForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None:
+            user = User(username=form.username.data, password=form.password.data)
+            db.session.add(user)
+            db.session.commit()
+        username = form.username.data
+        form.username.data = ''
+        form.password.data = ''
+        flash("User added Succesfully!")
+    our_users = User.query.all()
+    return render_template('addUser.html', form=form, username=username, our_users=our_users)
 
 @app.route("/")
 def index():
@@ -40,10 +63,6 @@ def login():
 def logout():
     session["name"] = None
     return redirect("/")
-
-
-
-
 
 
 @app.route('/poker', methods=["GET", "POST"])
