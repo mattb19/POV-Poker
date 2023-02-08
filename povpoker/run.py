3dc0f89701771f978e6fa1d1d8883ecdc0437abc
mattb19 <80365359+mattb19@users.noreply.github.com>
2023-02-08 17:50:13 -0500

    sessions and names work

--- povpoker/run.py
+++ povpoker/run.py
@@ -1,19 +1,22 @@
-import random
-import re
-import sys
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 from turbo_flask import Turbo
 import threading
 import time
 from Player import Player
 from main import Game
+from flask_session import Session
 
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+app.config['SECRET_KEY'] = 'secret!'
+Session(app)
 turbo = Turbo(app)
 
 player = [Player("Jeremy",None,None,1000,0,0), Player("Matt",None,None,1000,1,0), Player("Trent",None,None,1000,2,0), Player("Ryan",None,None,1000,3,0), Player("Jackson",None,None,1000,4,0), Player("Luke",None,None,1000,5,0), Player("David",None,None,1000,6,0), Player("Max",None,None,1000,6,0), Player("Ethan",None,None,1000,6,0), Player("Jack",None,None,1000,6,0)]
 game = Game(player, 10, 20)
 game.newRound()
+name = "Matt"
 
 @app.before_first_request
 def before_first_request():
@@ -25,7 +28,26 @@ def update_load():
             time.sleep(1)
             turbo.push(turbo.replace(render_template('table.html'), 'load'))
 
-@app.route('/')
+@app.route("/")
+def index2():
+    if not session.get("name"):
+        return redirect("/login")
+    return render_template('index.html')
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        session["name"] = request.form.get("name")
+        name = session["name"]
+        return redirect("/")
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session["name"] = None
+    return redirect("/")
+
+@app.route('/poker')
 def index():
     return render_template('index2.html')
 
@@ -35,7 +57,7 @@ def page2():
 
 @app.context_processor
 def inject_load():
-    return {'load1': game.getPot(), 'load5': game.getCurrentPlayer(), 'game' : game}
+    return {'load1': game.getPot(), 'name': name, 'game' : game}
 
 @app.route('/bet/', methods=["GET", "POST"])
 def getBets():
