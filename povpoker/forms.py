0788be5d911a386fb7d98753fb0cb70c36282261
mattb19 <matthew51672@gmail.com>
2023-04-18 18:10:30 -0400

    Nuke
    starting from scratch

--- povpoker/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, validators
-from wtforms.validators import DataRequired
-
-class UserForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired()])
-    password = StringField("Password", validators=[DataRequired()])
-    submit = SubmitField("Submit")
--- povpoker/models.py
+++
@@ -1,6 +0,0 @@
-from run import db
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(15), nullable=False)
-    password = db.Column(db.String(15), nullable=False)
