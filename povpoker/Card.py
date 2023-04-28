d0a60d3b0ac8704165d3d1d7e3c55277aad08502
mattb19 <matthew51672@gmail.com>
2023-04-27 21:06:38 -0400

    update

--- povpoker/Card.py
+++ povpoker/Card.py
@@ -19,6 +19,20 @@ class Card:
     
     def __str__(self) -> str:
         '''gfgg'''
+        cee = {
+            3 : "three",
+            2 : "two",
+            4 : "four",
+            5 : "five",
+            6 : "six",
+            7 : "seven",
+            8 : "eight",
+            9 : "nine",
+            "ace" : "ace",
+            "king" : "king",
+            "queen" : "queen",
+            "jack" : "jack",
+        }
         return "../static/PNG-cards-1.3/"+self._num+"_of_"+self._suit+".png"
 
     
